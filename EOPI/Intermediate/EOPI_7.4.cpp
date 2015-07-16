43141d8b5d1f8db49d13f8324f51110bd89a93f6
Sun77789 <aubakirova.m.m@gmail.com>
2015-07-15 22:10:43 -0700

    7.9 added iterative approach to reversing singly linked list

---
+++ EOPI/Intermediate/EOPI_7.4.cpp
@@ -0,0 +1,28 @@
+// Let hl and h2 be the heads of lists Ll and U, respectively.
+//  Assume that Ll and L2 are well-formed, 
+// that is each consists of a finite sequence of nodes. 
+// (In particular, neither list has a cycle.) 
+// How would you determine if there exists a node r reachable from both
+// hl and h2 by following the next fields? If such a node exists, 
+// find the node that appears earliest when traversing the lists. 
+// You are constrained to use no more than constant additional storage.
+
+
+// say first linked list ll1 is of size m
+// say second linked list ll2 is of size n
+/*
+
+I was too lazy to type it up, but if you are here I am guessing you need some ideas?
+1. Brute force search should be obvious and should take O(nm)
+Not great
+
+2. If descrtuctive updates are possible, you can alter the method for reversing 
+linked list and track down the common node
+
+3. If not, a better solution is to note that since there are no cycles
+and two linked lists share sequence of nodes, then  that sequence must be at the end.
+if m > n , Advance pointer  of ll1 by (m-n) nodes, and then
+start advancing pointers of both lists simultenenously comparing them
+if they are the same, then we found a first shared node!
+
+*/
--- EOPI/Intermediate/EOPI_7.9.cpp
+++ EOPI/Intermediate/EOPI_7.9.cpp
@@ -12,9 +12,10 @@
 #include "../../Data_Structs/Node.h"
 #include "../../Data_Structs/LinkedList.h"
 
+// **** Recursive approach (implicitly is using O(n) storage on the stack)
 Node* reverseLinkedListHelper(Node *head, Node **n_head)
 {
-	if(head->getNext() == NULL) {
+    if(head->getNext() == NULL) {
         // we got to the end
         // return the last node
         // and save it in *n_head
@@ -25,7 +26,7 @@ Node* reverseLinkedListHelper(Node *head, Node **n_head)
 
     // n_node in this case will be a next node 
     // of the current node in the original linkedlist
-	Node *n_node = reverseLinkedListHelper(head->getNext(), n_head);
+    Node *n_node = reverseLinkedListHelper(head->getNext(), n_head);
     // now next node should point to the current node
     n_node->setNext(head);
     // this is mostly done for the last node 
@@ -49,6 +50,21 @@ Node* reverseLinkedList(Node *head)
     return n_head;
 }
 
+// **** Iterative approach O(n) time
+Node *reverseLinkedListIterative(Node *head)
+{
+    Node *prev = NULL, *curr = head, *temp = NULL;
+    while(curr) {
+        temp = curr->getNext();
+        curr->setNext(prev);
+        prev = curr;
+        curr = temp;
+    }
+
+    return prev;
+}
+
+
 int main()
 {
     LinkedList ll;
@@ -60,8 +76,15 @@ int main()
     ll.print();
     // Output:
     // Before: 1 2 3 4
-    std::cout<<"Reversed: ";
+    std::cout<<"Reversed using Recursive approach: ";
     ll.setHead(reverseLinkedList(ll.getHead()));
     ll.print();
+    // Now: 4 3 2 1
+    // ______________________________________________
+    // reverse again
+    std::cout<<"Reversed using Iterative approach: ";
+    ll.setHead(reverseLinkedListIterative(ll.getHead()));
+    ll.print();
+    // Back: 1 2 3 4
     return 0;
 }
